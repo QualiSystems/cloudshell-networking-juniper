9ae6a9032bafbab3ea75286cc1f1bc3b0668667e
Yaroslav Nikonorov <yaroslav.n@quali.com>
2020-01-10 12:36:57 +0200

    Added extras connectivity

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     packages=find_packages(),
     install_requires=required,
     tests_require=required_for_tests,
+    extras_require={"connectivity": ["cloudshell-shell-connectivity-flow>=1.0,<2"]},
     version=version_from_file,
     description="QualiSystems Juniper package",
     include_package_data=True,
